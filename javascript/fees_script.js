310fd7832f8df4d5fa39c5296608ee99f237de40
Sriharikondaparthi <sriharikondaparthi@gmail.com>
2025-02-14 21:13:46 +0530

    Add files via upload

---
+++ javascript/fees_script.js
@@ -0,0 +1,190 @@
+function getInputValue(){
+
+    let inputVal=document.getElementById("uid").value;
+    let a=1;
+
+    if(inputVal==1){
+        document.getElementById("demo").innerHTML ="Srihari kondaparthi";
+        document.getElementById("demo1").innerHTML ="1000";
+    }
+    else if(inputVal==2)
+    {
+        document.getElementById("demo").innerHTML ="No Records Found";
+        document.getElementById("demo1").innerHTML ="0";  
+    }
+    else if(inputVal==3)
+    {
+        document.getElementById("demo").innerHTML ="Rakshitha";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+
+    else if(inputVal==4)
+    {
+        document.getElementById("demo").innerHTML ="Raghavendra";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==5)
+    {
+        document.getElementById("demo").innerHTML ="Saketh";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==6)
+    {
+        document.getElementById("demo").innerHTML ="Shashank";
+        document.getElementById("demo1").innerHTML ="20000";   
+    }
+    else if(inputVal==7)
+    {
+        document.getElementById("demo").innerHTML ="Shivaprasad";
+        document.getElementById("demo1").innerHTML ="20000";   
+    }
+    else if(inputVal==8)
+    {
+        document.getElementById("demo").innerHTML ="Sai kiran";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==9)
+    {
+        document.getElementById("demo").innerHTML ="Aravind";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==10)
+    {
+        document.getElementById("demo").innerHTML ="Mahesh";
+        document.getElementById("demo1").innerHTML ="7500";   
+    }
+
+     else if(inputVal==11)
+    {
+        document.getElementById("demo").innerHTML ="Ravi";
+        document.getElementById("demo1").innerHTML ="0";   
+    }
+     else if(inputVal==12)
+    {U
+        document.getElementById("demo").innerHTML ="Srikanth";
+        document.getElementById("demo1").innerHTML ="20000";   
+    }
+    else if(inputVal==13)
+    {
+        document.getElementById("demo").innerHTML ="Bharath";
+        document.getElementById("demo1").innerHTML ="7500";   
+    }  
+    else if(inputVal==14)
+    {
+        document.getElementById("demo").innerHTML ="Prashanth";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==15)
+    {
+        document.getElementById("demo").innerHTML ="Harish";
+        document.getElementById("demo1").innerHTML ="2500";   
+    }
+    else if(inputVal==16)
+    {
+        document.getElementById("demo").innerHTML ="Pavan";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==17)
+    {
+        document.getElementById("demo").innerHTML ="Naveen";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+
+    else if(inputVal==18)
+    {
+        document.getElementById("demo").innerHTML ="Varun";
+        document.getElementById("demo1").innerHTML ="20000"; 
+    }
+    else if(inputVal==19)
+    {
+        document.getElementById("demo").innerHTML ="Vijay";
+        document.getElementById("demo1").innerHTML ="0"; 
+    }
+    else if(inputVal==20)
+    {
+        document.getElementById("demo").innerHTML ="Kishore";
+        document.getElementById("demo1").innerHTML ="2500"; 
+    }
+     else if(inputVal==21)
+    {
+        document.getElementById("demo").innerHTML ="Naresh";
+        document.getElementById("demo1").innerHTML ="1000"; 
+    }
+    else if(inputVal==22)
+    {
+        document.getElementById("demo").innerHTML ="Ramesh";
+        document.getElementById("demo1").innerHTML ="20000"; 
+    }
+    else if(inputVal==23)
+    {
+        document.getElementById("demo").innerHTML ="Anil";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+    else if(inputVal==24)
+    {
+        document.getElementById("demo").innerHTML ="Anji";
+        document.getElementById("demo1").innerHTML ="0"; 
+    }
+    else if(inputVal==25)
+    {
+        document.getElementById("demo").innerHTML ="Ram";
+        document.getElementById("demo1").innerHTML ="7500"; 
+    }
+    else if(inputVal==26)
+    {
+        document.getElementById("demo").innerHTML ="Muni";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+     else if(inputVal==27)
+    {
+        document.getElementById("demo").innerHTML ="Abhiram";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+    else if(inputVal==28)
+    {
+        document.getElementById("demo").innerHTML ="Khalid";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+
+    else if(inputVal==29)
+    {
+        document.getElementById("demo").innerHTML ="Sameer";
+        document.getElementById("demo1").innerHTML ="0"; 
+    }
+     else if(inputVal==30)
+    {
+        document.getElementById("demo").innerHTML ="Adithya";
+        document.getElementById("demo1").innerHTML ="0"; 
+    }
+    else if(inputVal==31)
+    {
+        document.getElementById("demo").innerHTML ="Vignesh";
+        document.getElementById("demo1").innerHTML ="2500"; 
+    }
+    else if(inputVal==32)
+    {
+        document.getElementById("demo").innerHTML ="Avinash";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+    else if(inputVal==33)
+    {
+        document.getElementById("demo").innerHTML ="Amruth";
+        document.getElementById("demo1").innerHTML ="7500"; 
+    }
+    else if(inputVal==34)
+    {
+        document.getElementById("demo").innerHTML ="Renu";
+        document.getElementById("demo1").innerHTML ="20000 + 2500 = 22500"; 
+    }
+    else if(inputVal==35)
+    {
+        document.getElementById("demo").innerHTML ="srivani";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+    else{
+        document.getElementById("demo").innerHTML ="No Records Found";
+        document.getElementById("demo1").innerHTML ="0";
+    }
+    
+}
+
